2b2f1f049bf10eae8a0c49f44cc9d843756f8584
Selvira18 <37258333+Selvira18@users.noreply.github.com>
2018-04-17 18:47:46 +0700

    Add files via upload

---
+++ fungsi kalkulator.py
@@ -0,0 +1,10 @@
+print ("______KALKULATOR_________");
+def kalkulator ():
+    def tambah():
+        print ('1.penjumlahan')
+        a=input('Masukkan nilai a =')
+        b=input('Masukkan nilai b =')
+        c = a+b
+        print ('a+b='),c
+        print (' ')
+        tanya()
